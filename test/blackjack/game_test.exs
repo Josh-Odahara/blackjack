6823ee07316b14937f8c6c55c0b058f553aaf584
Josh Odahara <josh.odahara@gmail.com>
2026-06-19 00:16:29 -0700

    Add ExUnit testing

--- test/blackjack/game_test.exs
+++ test/blackjack/game_test.exs
@@ -32,4 +32,18 @@ test "shuffle_deck/1 checks for 52 cards and the sorted version match" do
   assert Enum.sort(shuffled) == Enum.sort(deck)
 end
 
+test "card/value returns correct values" do
+  assert Game.card_value({:hearts, 2}) == 2
+  assert Game.card_value({:spades, :king}) == 10
+  assert Game.card_value({:diamonds, :ace}) == 11
+end
+
+test "hand/value/1 returns correct values for two cards in hand" do
+  assert Game.hand_value([{:hearts, 5}, {:spades, :jack}]) == 15
+  assert Game.hand_value([{:hearts, :ace}, {:spades, :jack}]) == 21
+  assert Game.hand_value([{:hearts, :ace}, {:spades, 10}, {:diamonds, 2}, {:clubs, :queen}]) == 23
+end
+
+
+
 end
